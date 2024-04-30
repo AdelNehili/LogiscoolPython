7f380ea27f80c3b26326f2a9cf888625fb5b0edf
AdelNehili <adel@LAPTOP-435R1L8O>
2024-04-30 09:46:20 +0200

    small modif

--- git_bash.sh
+++ git_bash.sh
@@ -1,7 +1,14 @@
-echo "# LogiscoolPython" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/AdelNehili/LogiscoolPython.git
-git push -u origin main
+if [ $1 == "git_init" ]; then
+    echo "# LogiscoolPython" >> README.md
+    git init
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/AdelNehili/LogiscoolPython.git
+    git push -u origin main
+
+elif [ $1 == "hello" ]; then
+    echo "Hello"
+else
+    echo "Error, don't recognise this command:"
+fi
